9d26801c2b53d2d7d63fec46ecd8830d489245fb
Lambert <1841302343@qq.com>
2020-12-30 00:36:18 +0800

    233
    233

---
+++ Java/test1.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * VIDI VINI VIKI                                                             *
+ * Copyright © 18:15:39 $today.year-$today.month-29  Lambert All rights reserved.
+ *                                                                            *
+ * Love Liya Forever!
+ ******************************************************************************/
+
+package Test;
+public class test1{
+
+}
