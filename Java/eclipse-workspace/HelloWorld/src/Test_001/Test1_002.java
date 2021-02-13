74585809c1838c43cd5c0b05500cde799279e89e
Lambert <1841302343@qq.com>
2021-02-13 12:22:58 +0800

    Change

---
+++ Java/eclipse-workspace/HelloWorld/src/Test_001/Test1_002.java
@@ -0,0 +1,14 @@
+package Test_001;
+
+public class Test1_002 {
+
+	public Test1_002() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
