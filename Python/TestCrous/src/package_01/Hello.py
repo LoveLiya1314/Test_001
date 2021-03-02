fb99f9856dfa4070e045856db5fa7e611cc0b5c1
Lambert <1841302343@qq.com>
2021-03-02 23:17:08 +0800

    py

---
+++ Python/TestCrous/src/package_01/Hello.py
@@ -0,0 +1,21 @@
+
+import math
+
+import PIL
+
+
+
+def circle(r):
+    print(math.pi*r*r)
+circle(20)
+
+import sys
+for i in range(0,10):
+    print(sys.path[i])
+
+
+from PIL import Image
+from PIL import ImageFilter
+im = Image.open('test.jpg')
+e33 = im.filter(ImageFilter.CONTOUR)
+e33.save('E(33).jpg')
