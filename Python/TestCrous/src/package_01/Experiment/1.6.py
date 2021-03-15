2d03ea516cbe65b2d6bc364ba8cc4c439ede4e73
Lambert <1841302343@qq.com>
2021-03-15 21:31:18 +0800

    Pyrhon turtle Change

--- Python/TestCrous/src/package_01/Experiment/1.6.py
+++ Python/TestCrous/src/package_01/Experiment/1.6.py
@@ -1,5 +1,18 @@
 # -*-coding:UTF-8-*-
-from turtle import *
-for i in range(500): #重复500次
-  forward(i)
-  left(90)
+# from turtle import *
+# for i in range(500): #重复500次
+#   forward(i)
+#   left(90)
+
+import turtle
+from time import sleep
+t = turtle.Pen()
+turtle.bgcolor("grey")
+sides=6
+colors=["red","yellow","green","blue","orange","purple"]
+for x in range(360):
+    t.pencolor(colors[x%sides])
+    t.forward(x*3/sides+x)
+    t.left(360/sides+1)
+    t.width(x*sides/200)
+  
