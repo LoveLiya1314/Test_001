fd50f6bd974a529b5c20d72b7d0897e63b906a77
1841302343@qq.com <1841302343@qq.com>
2021-03-16 17:17:08 +0800

    Change ---Python

--- Python/TestCrous/src/package_01/Experiment/1.6.py
+++ Python/TestCrous/src/package_01/Experiment/1.6.py
@@ -5,14 +5,15 @@
 #   left(90)
 
 import turtle
-from time import sleep
 t = turtle.Pen()
 turtle.bgcolor("grey")
+#sides=evali(input("输入要绘制的边的数目，请输入2-6的数字！"))
 sides=6
 colors=["red","yellow","green","blue","orange","purple"]
 for x in range(360):
     t.pencolor(colors[x%sides])
     t.forward(x*3/sides+x)
     t.left(360/sides+1)
-    t.width(x*sides/200)
-  
+    t.width(x*sides/180)
+    t.left(91)
+print("####结束####")
