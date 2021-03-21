2facc3b0fabb6c3cc5695334acec279417184d5b
1841302343@qq.com <1841302343@qq.com>
2021-03-21 11:02:58 +0800

    Dollar to RMB andRMB to Dollar

--- Python/TestCrous/src/package_01/Experiment/1.7.py
+++ Python/TestCrous/src/package_01/Experiment/1.7.py
@@ -1,1 +1,15 @@
 #-*- coding:utf-8-*-
+ #e1.1 TempConvert.py
+GetStr=input("请输入带符号的金额")
+if GetStr[-1] in ['$']:
+	RMB=(eval(GetStr[0:-1])*6)
+	print("转换后的人民币是{:.2f}RMB".format(RMB))
+elif GetStr[-1] in ['￥']:
+	Dollar=eval(GetStr[0:-1])/6
+	print("转换后的美金是{:.2f}Dollar".format(Dollar))
+else:
+	print("输入格式错误")
+
+
+
+
