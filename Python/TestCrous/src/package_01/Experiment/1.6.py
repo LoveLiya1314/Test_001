7d0c90a0cde46dcf40c628465e2d0fcfb388f59f
Lambert <1841302343@qq.com>
2021-04-03 17:37:21 +0800

    change

--- Python/TestCrous/src/package_01/Experiment/1.6.py
+++ Python/TestCrous/src/package_01/Experiment/1.6.py
@@ -7,15 +7,24 @@
 import turtle
 t = turtle.Pen()
 turtle.bgcolor("grey")
+<<<<<<< Updated upstream
 t.speed(10)
 #sides=evali(input("输入要绘制的边的数目，请输入2-6的数字！"))
 sides=6################################6,7,8
 colors=["red","yellow","green","blue","orange","purple","black","pink"]
+=======
+#sides=evali(input("输入要绘制的边的数目，请输入2-6的数字！"))
+sides=6
+colors=["red","yellow","green","blue","orange","purple"]
+>>>>>>> Stashed changes
 for x in range(360):
     t.pencolor(colors[x%sides])
     t.forward(x*3/sides+x)
     t.left(360/sides+1)
     t.width(x*sides/180)
     t.left(91)
+<<<<<<< Updated upstream
 turtle.exitonclick()
+=======
+>>>>>>> Stashed changes
 print("####结束####")
