314bcd47949c536ce00c39e5d5a2085c50342f29
1841302343@qq.com <1841302343@qq.com>
2021-04-08 20:39:25 +0800

    Change MD

--- Java/test1.java
+++ Java/test1.java
@@ -1,3 +1,7 @@
+/*
+ *   Copyright (c) 2021 
+ *   All rights reserved.
+ */
 /******************************************************************************
  * VIDI VINI VIKI                                                             *
  * Copyright © 18:15:39 $today.year-$today.month-29  Lambert All rights reserved.
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Test_001
 在这个Test分支里，我们进行了一系列有关C++、Java、Python、C、PHP、R、Julia、Html、CSS、JavaScript、Go以及其他各种语言的入门测试代码
 完全做测试的代码
+基本上没有借鉴价值
