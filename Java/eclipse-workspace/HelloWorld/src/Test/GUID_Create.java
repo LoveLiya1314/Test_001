a34249c52d46f037546ce35f8013301918e2d957
Lambert <Loveliya1314@foxmail.com>
2021-07-15 12:43:51 +0800

    Web Create

---
+++ Java/eclipse-workspace/HelloWorld/src/Test/GUID_Create.java
@@ -0,0 +1,22 @@
+package Test;
+import java.util.UUID;
+
+
+public class GUID_Create {
+
+	public GUID_Create() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		  UUID uuid = UUID.randomUUID();   
+		  System.out.println (uuid); 
+		 //System.out.println(UUID.randomUUID().toString().replace("-", ""));
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
